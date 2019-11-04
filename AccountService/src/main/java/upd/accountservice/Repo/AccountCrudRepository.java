3e210e194797201692b4709a08243b443be74f1c
JeroenPeters96 <jeroen.peters1@hotmail.nl>
2019-11-04 02:21:18 +0100

    Merge branch 'master' of C:\Users\Jeroen\IdeaProjects\UpkeepDrawRedone with conflicts.

--- AccountService/src/main/java/upd/accountservice/Repo/AccountCrudRepository.java
+++ AccountService/src/main/java/upd/accountservice/Repo/AccountCrudRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import upd.accountservice.Models.Account;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface AccountCrudRepository extends CrudRepository<Account,String> {
 
